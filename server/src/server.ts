6992b27a9c6eb2acbfe048f01c903ed5f9b11704
jefshandler <jef_shandler@msn.com>
2022-05-14 13:56:05 -0300

    final aula 3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,15 @@
 
+import cors from 'cors';
 import express from 'express';
 
 import { routes } from './routes';
 
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+
 
 // app.post('/feedbacks', (req, res) => {
 //     console.log(req.body);
